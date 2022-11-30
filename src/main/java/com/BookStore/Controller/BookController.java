255a7726847ef24bbc6210a94d0f966573570c39
Chinthireddy Sindhu <chinthi.s@accolite.com>
2022-11-30 15:44:18 +0530

    BookStore Application

---
+++ src/main/java/com/BookStore/Controller/BookController.java
@@ -0,0 +1,30 @@
+package com.BookStore.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.BookStore.Model.Book;
+
+import com.BookStore.Services.BookService;
+
+@RestController
+public class BookController {
+	@Autowired
+	private BookService bs;
+	
+	@PostMapping("/books")
+	private  ResponseEntity<Book> saveBook(@RequestBody Book b){
+		return ResponseEntity.ok().body(this.bs.addBook(b));
+	}
+	@GetMapping("/books")
+	private  ResponseEntity<List<String>> getAllBooks(){
+		return ResponseEntity.ok(this.bs.searchAllBooks());
+	}
+	@GetMapping("/books/{bid}")
+	private  ResponseEntity<String> getBook(@PathVariable int bid){
+		return ResponseEntity.ok(this.bs.searchBook(bid));
+	}
+}
---
+++ src/main/java/com/BookStore/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.BookStore.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.BookStore.Model.User;
+
+public interface UserRepository extends JpaRepository<User,Integer>
+{
+
+	
+}
